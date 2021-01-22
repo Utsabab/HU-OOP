ecdf4391dd51520d7f8c5edabccdbf948c74ec8a
Utsab <Utsab@192.168.1.80>
2021-01-21 17:51:50 -0800

    Added name in the comment

--- HelloWorld/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ HelloWorld/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -1,8 +1,11 @@
+// Utsab Khakurel
+
 package org.howard.edu.lsp.assignment1;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		//prints "Hello World!!"
 		System.out.println("Hello World!!");
 	}
 
